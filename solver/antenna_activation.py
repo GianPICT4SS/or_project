66dbea8c720212c9ab24fbeedc4e820e735f88c5
GianP <gpdomiziani@gmail.com>
2020-05-12 12:48:12 +0200

    Simple Antenna Activation analytic problem solved. Class ConflictGraph with its methods implemented.

--- solver/antenna_activation.py
+++ solver/antenna_activation.py
@@ -6,29 +6,29 @@ import numpy as np
 
 
 class SimpleAntennaActivation():
-    def __init__(self, dict_data):
+    def __init__(self):
         pass
 
+
     def solve(
-        self, dict_data, time_limit=None,
-        gap=None, verbose=False
+            self, dict_data, time_limit=None,
+            gap=None, verbose=False
     ):
         """[summary]
-        
+
         Arguments:
             dict_data {[type]} -- [description]
-        
+
         Keyword Arguments:
             time_limit {[type]} -- [description] (default: {None})
             gap {[type]} -- [description] (default: {None})
             verbose {bool} -- [description] (default: {False})
-        
+
         Returns:
             [type] -- [description]
         """
         logging.info("######### Simple Antenna Activation ###########")
         items = range(dict_data['n_items'])
-        #pro = [0, 10, 20, 13]
 
 
         x = LpVariable.dicts(
@@ -44,22 +44,16 @@ class SimpleAntennaActivation():
 
         prob = LpProblem(problem_name, LpMaximize)
         prob += lpSum([dict_data['profits'][i] * x[i] for i in items]), "obj_func"
-        #prob += lpSum([pro[i] * x[i] for i in items]), "obj_func"
-        #prob += lpSum([dict_data['sizes'][i] * x[i] for i in items]) <= dict_data['max_size'], "max_vol"
+        # prob += lpSum([pro[i] * x[i] for i in items]), "obj_func"
+        # prob += lpSum([dict_data['sizes'][i] * x[i] for i in items]) <= dict_data['max_size'], "max_vol"
         for i in range(len(dict_data['A'])):
             """Do not sum all constraints: splits constraints"""
-            #prob += lpSum(x[i] + x[j] for i, j in zip(*(dict_data['A'], dict_data['B']))) <= 1, "max_vol"
+            # prob += lpSum(x[i] + x[j] for i, j in zip(*(dict_data['A'], dict_data['B']))) <= 1, "max_vol"
             k = dict_data['A'][i]
             j = dict_data['B'][i]
-            prob += lpSum(x[k]+x[j]) <= 1, f"conflict_{i}"
+            prob += lpSum(x[k] + x[j]) <= 1, f"conflict_{i}"
 
-        print(f'profits: {dict_data["profits"]}')
-        #print(f'profits: {pro}')
-        a = [x[i]+x[j] for i, j in zip(*(dict_data['A'], dict_data['B']))]
-        print(f'constraints: {a}')
-        for c, d in zip(*(dict_data['A'], dict_data['B'])):
-            print(f'conflicts: {c, d}')
-
+        logging.info(f'profits: {dict_data["profits"]}')
         prob.writeLP("./logs/{}.lp".format(problem_name))
 
         msg_val = 1 if verbose else 0
@@ -82,4 +76,3 @@ class SimpleAntennaActivation():
         )
         logging.info("#########")
         return of, sol_x, comp_time, x, prob
-
