7939d559cbe0fe09c8f9a295ca6ad080c9460f57
GianP <gpdomiziani@gmail.com>
2020-05-12 12:48:12 +0200

    Simple Antenna Activation analytic problem solved. Class ConflictGraph with its methods implemented.

--- main.py
+++ main.py
@@ -2,11 +2,43 @@ import json
 import logging
 import numpy as np
 from simulator.instance import Instance
-from solver.simpleKnapsack import SimpleAntennaActivation
+from solver.antenna_activation import SimpleAntennaActivation
 from heuristic.simpleHeu import SimpleHeu
 import networkx as net
 
 np.random.seed(0)
+
+class ConflictGraph():
+
+    def __init__(self, dict_data, p):
+
+        self.nodes = np.arange(dict_data['n_items'])
+        self.p = p
+        self.dict_data = dict_data
+        self.Graph = net.Graph()
+
+    def simple_conflict_graph(self, conflict=10):
+        """build a simple conflict graph: without particular assumptions, a conflict graph is built,
+        with a fixed number of conflict and using as antennas two"""
+
+        while len(list(self.Graph.edges)) < conflict:
+
+            i = np.random.randint(low=np.min(self.nodes), high=np.max(self.nodes))
+            j = np.random.randint(low=np.min(self.nodes), high=np.max(self.nodes))
+            #logging.info(f'(i,j) = {i,j}')
+            if i != j and j+i < 2*len(self.nodes):
+            # make sure that the two random nodes are different in order to create a conflict edge, without
+            # taking a nodes not present in the possible nodes list.
+                edge = (i, j)
+                #logging.info(f'Adding edge: {edge}')
+                self.Graph.add_edge(*edge)
+                logging.info(f' edges: {self.Graph.edges}')
+
+        # Take the conflict edges
+        a, b = zip(*self.Graph.edges)
+        self.dict_data['A'] = [x for x in a]
+        self.dict_data['B'] = [x for x in b]
+        return self
 
 
 
@@ -28,23 +60,12 @@ if __name__ == '__main__':
     )
     dict_data = inst.get_data()
 
-    G = net.Graph(name='SimpleAntenna')
-
-    ls = np.arange(dict_data['n_items'])
-    for i in ls:
-        edge = (i, i + 1)
-        G.add_edge(*edge)
-
-    a, b = zip(*G.edges)
-    dict_data['A'] = [x for x in a]
-    dict_data['B'] = [x for x in b]
-    dict_data['B'][dict_data['n_items']-1] = 1
-
-
+    graph = ConflictGraph(dict_data, p=0.8)
+    graph.simple_conflict_graph(conflict=graph.dict_data['n_items'])
 
     prb = SimpleAntennaActivation()
     of_exact, sol_exact, comp_time_exact, x, prob = prb.solve(
-        dict_data,
+        graph.dict_data,
         verbose=True
     )
     print(of_exact, sol_exact, comp_time_exact)
