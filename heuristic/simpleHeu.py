9c4627eb996652a7d1f28c32ae9a7367731fe7be
GianP <gpdomiziani@gmail.com>
2020-05-20 14:38:46 +0200

    simple Heu fixed (no cg edges)

--- heuristic/simpleHeu.py
+++ heuristic/simpleHeu.py
@@ -5,6 +5,10 @@ import numpy as np
 import logging
 from pulp import *
 
+from queue import Queue
+
+Q = Queue()
+
 log_name = "./logs/main.log"
 logging.basicConfig(
     filename=log_name,
@@ -14,7 +18,7 @@ logging.basicConfig(
 )
 
 class SimpleHeu():
-    def __init__(self, n, graph):
+    def __init__(self, n, graph, dict_data):
         """
 
         :param n: iteration number
@@ -22,36 +26,13 @@ class SimpleHeu():
         """
         self.n = n
         self.graph = graph
-        self.tot_u = np.linalg.norm(graph.dict_data['profits'])
-        #self.init_CGnodes_ls = list(graph.randomGraph.nodes)
-        self.init_CGnodes_ls = np.unique(graph.dict_data['R_A'] + graph.dict_data['R_B'])
+        self.dict_data = dict_data
+        self.nodes_deleted = Queue()
+        self.tot_u = np.linalg.norm(dict_data['profits'])
+        self.init_CGnodes_ls = list(graph.nodes)
+        #self.init_CGnodes_ls = np.unique(graph.dict_data['R_A'] + graph.dict_data['R_B'])
         logging.info(f'init_CGnodes: {self.init_CGnodes_ls}')
 
-    def solve(
-        self, dict_data
-    ):
-        """[summary]
-        
-        Arguments:
-            dict_data {[type]} -- [description]
-        
-        Keyword Arguments:
-            time_limit {[type]} -- [description] (default: {None})
-            gap {[type]} -- [description] (default: {None})
-            verbose {bool} -- [description] (default: {False})
-        
-        Returns:
-            [type] -- [description]
-        """
-        sol_x = [0] * dict_data['n_items']
-        start = time.time()
-        sol_x[self.n] = math.ceil(dict_data['max_size'] / dict_data['sizes'][0])
-        end = time.time()
-        
-        of = dict_data['profits'][self.n] * sol_x[self.n]
-        comp_time = end - start
-        
-        return of, sol_x, comp_time
 
     def recursive_cg_solve(self, mu=1):
         """
@@ -62,51 +43,75 @@ class SimpleHeu():
         """
 
 
-        logging.info(f'heu_graph nodes: {self.graph.heu_graph.nodes}')
+        logging.info(f'heu_graph nodes: {self.graph.nodes}')
         #nodes_ls = list(self.graph.Graph.nodes)  # Get all nodes in CG
-        u = (self.graph.dict_data['profits'][self.init_CGnodes_ls[self.n]])  # compute the Utility of the first node in the CG
+        u = (self.dict_data['profits'][self.init_CGnodes_ls[self.n]])  # compute the Utility of the first node in the CG
         logging.info(f'profits of {self.init_CGnodes_ls[self.n]}: {u}')
 
-        nodes_conf_n = list(self.graph.heu_graph[self.init_CGnodes_ls[self.n]])  # get all nodes in conflict with the node n
-        logging.info(f'nodes conflict: {nodes_conf_n}')
-        logging.info(f"utilities conflict nodes: {self.graph.dict_data['profits'][nodes_conf_n]}")
-        conflicts = len(nodes_conf_n)  # compute with how many nodes n is in conflict
-        logging.info(f'len conflicts: {conflicts}')
+        try:
+            nodes_conf_n = list(self.graph[self.init_CGnodes_ls[self.n]])  # get all nodes in conflict with the node n
+            logging.info(f'nodes conflict: {nodes_conf_n}')
+            logging.info(f"utilities conflict nodes: {self.dict_data['profits'][nodes_conf_n]}")
+            conflicts = len(nodes_conf_n)  # compute with how many nodes n is in conflict
+            logging.info(f'len conflicts: {conflicts}')
+        except:
+            self.n = self.n + 1
+            if self.n < len(self.init_CGnodes_ls):
+                return self.recursive_cg_solve()
+            else:
+                return self
+
+        #nodes_conf_n_utilities = self.dict_data['profits'][nodes_conf_n]
 
         ut_conf = 0  # initialize the total utility of the nodes in conflict with n
         # compute the total utility of the nodes in confict with n
         for c in nodes_conf_n:
-            ut_conf = ut_conf + self.graph.dict_data['profits'][c]
+            ut_conf = ut_conf + self.dict_data['profits'][c]
         #ut_conf = ut_conf/self.tot_u
 
         if ut_conf > u*conflicts*mu:
             logging.info(f'ut_conf: {ut_conf}; conflicts*u*mu: {conflicts*u*mu}')
-            self.graph.heu_graph.remove_node(self.init_CGnodes_ls[self.n])  # remove node n from the solution
+            self.graph.remove_node(self.init_CGnodes_ls[self.n])  # remove node n from the solution
+            self.nodes_deleted.put(self.init_CGnodes_ls[self.n])
             logging.info(f'nodes {self.init_CGnodes_ls[self.n]} turn OFF')
             # Recursion
             self.n = self.n + 1
             if self.n < len(self.init_CGnodes_ls):  # check if all nodes have been visited
                 return self.recursive_cg_solve()
             else:
-                self.graph.dict_data['Heu_sol'] = np.sort(list(self.graph.heu_graph.nodes))
+                self.dict_data['Heu_sol'] = np.sort(list(self.graph.nodes))
                 return self
-        else:  # do not exclude nodes n
+        else:  # do not exclude nodes n but others
+
+            self.graph.remove_nodes_from(nodes_conf_n)
+            self.nodes_deleted.put(nodes_conf_n)
             # Recursion
-            logging.info(f'nodes {self.init_CGnodes_ls[self.n]} turn ON')
+            logging.info(f'nodes {nodes_conf_n} turn OFF')
             self.n = self.n + 1
             if self.n < len(self.init_CGnodes_ls):
-                return self.recursive_cg_solve()
+               return self.recursive_cg_solve()
             else:
-                self.graph.dict_data['Heu_sol'] = np.sort(list(self.graph.heu_graph.nodes))
+                self.dict_data['Heu_sol'] = np.sort(list(self.graph.nodes))
                 return self
 
     def get_oFunction(self):
 
         obj_function = 0
-        sol_h = list(self.graph.heu_graph.nodes)
+        sol_h = list(self.graph.nodes)
         for s in sol_h:
-            obj_function += self.graph.dict_data['profits'][s]
+            obj_function += self.dict_data['profits'][s]
         return obj_function
+
+    def join_variables(self):
+
+        items = list(range(self.dict_data['n_items']))
+        Q.put(items)
+
+        deleted = Q.get(self.nodes_deleted)
+
+        final_sol = Q.queue
+
+        return final_sol
 
 
 
--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class ConflictGraph():
         self.dict_data = dict_data
         self.Ograph = net.Graph()
         self.randomGraph = net.Graph()
-        self.heu_graph = self.randomGraph
+        self.heu_graph = net.Graph()
+        self.heu_graph_r = net.Graph()
         self.info = {}
 
     def simple_conflict_graph(self, conflict=10):
@@ -37,6 +38,7 @@ class ConflictGraph():
                 edge = (i, j)
                 logging.info(f'Adding edge: {edge}')
                 self.Ograph.add_edge(*edge)
+                self.heu_graph.add_edge(*edge)
         logging.info(f' edges: {self.Ograph.edges}')
 
         # Take the conflict edges
@@ -52,15 +54,16 @@ class ConflictGraph():
         logging.info(f' p: {p}')
 
         for i in range(N):
-            for j in range(N-1):
-                #if i != j:
-                a = np.random.randint(low=0, high=N) / N
-                #logging.info(f' random a: {a}')
-                if a <= p:
-                    logging.info(f' random a: {a}')
-                    edge = (i, j+1)
-                    self.randomGraph.add_edge(*edge)
-                    #logging.info(f' Random edges: {self.Graph.edges}')
+            for j in range(N):
+                if i != j:
+                    a = np.random.randint(low=0, high=N) / N
+                    #logging.info(f' random a: {a}')
+                    if a <= p:
+                        logging.info(f' random a: {a}')
+                        edge = (i, j)
+                        self.randomGraph.add_edge(*edge)
+                        self.heu_graph_r.add_edge(*edge)
+                        #logging.info(f' Random edges: {self.Graph.edges}')
 
         logging.info(f'Random edges: {self.randomGraph.edges}')
         # Take the conflict edges
@@ -97,9 +100,13 @@ class ConflictGraph():
         deg_h = dict(self.heu_graph.degree())
         top5_h = sorted(deg_h.items(), key=lambda x: x[1], reverse=True)[:5]
 
+        deg_h_r = dict(self.heu_graph_r.degree())
+        top5_h_r = sorted(deg_h_r.items(), key=lambda x: x[1], reverse=True)[:5]
+
         self.info['top5Degree_O'] = top5_o
         self.info['top5Degree_R'] = top5_R
-        self.info['top4Degree_H'] = top5_h
+        self.info['top5Degree_H'] = top5_h
+        self.info['top5Degree_H_R'] = top5_h_r
         self.info['Utilities'] = self.dict_data['profits']
 
         return self
@@ -149,7 +156,9 @@ if __name__ == '__main__':
     print(f"of_exact: {of_exact}\n sol_exact: {sol_exact}\n comp_time_exact: {comp_time_exact}")
     print(f"of_exactR: {of_exactR}\n sol_exactR: {sol_exactR}\n comp_time_exactR: {comp_time_exactR}")
 
-    heu = SimpleHeu(graph=graph, n=0)
+    # standard
+    logging.info('*****Standard Heuristic Start********')
+    heu = SimpleHeu(graph=graph.heu_graph, dict_data=graph.dict_data, n=0)
     #of_heu, sol_heu, comp_time_heu = heu.solve(
     #   dict_data
     #)
@@ -158,6 +167,17 @@ if __name__ == '__main__':
     end = time.time()
     ob_heu = sheu.get_oFunction()
     print(f'Heuristic time: {end-start} \n', f'sol_heu: {ob_heu}')
+
+    #random CG
+    r_heu = SimpleHeu(graph=graph.heu_graph_r, dict_data=dict_data, n=0)
+    start = time.time()
+    logging.info('*******Random Heuristic Start*********')
+    r_s_heu = r_heu.recursive_cg_solve()
+    end = time.time()
+    ob_heu_r = r_s_heu.get_oFunction()
+    print(f'Random Heuristic time: {end - start} \n', f'Random sol_heu: {ob_heu_r}')
+
+
     #print(of_heu, sol_heu, comp_time_heu)
     graph.info_graph()
     # printing results of a file
