49507b1410e7e7622ada1a974b344cabbf881805
salvobellino95 <salvobellino95@gmail.com>
2020-06-15 23:40:49 +0200

    WIP ottimizzazione wgheu

--- heuristic/WGHeu.py
+++ heuristic/WGHeu.py
@@ -4,6 +4,17 @@ import networkx as nx
 import numpy as np
 #paper --> http://www.ru.is/~mmh/papers/WIS_WG.pdf
 
+class WGHeuNode():
+
+    def __init__(self, node_index, weighted_degree):
+        self.node_index = node_index
+        self.weighted_degree = weighted_degree
+        self.is_available = True
+
+    def __lt__(self, other):
+         return self.weighted_degree < other.weighted_degree        
+        
+
 class WGHeu():
 
     def solve(
@@ -50,3 +61,56 @@ class WGHeu():
         end = time.time()
         of = np.array([graph.nodes[u]['weight'] for u in graph.nodes]).dot(IS)
         return of, IS.tolist(), end - start,
+
+    def solveOptimized(
+        self, graph, profits
+    ):
+        """[summary]
+        
+        Arguments:
+            dict_data {[type]} -- [description]
+        
+        Keyword Arguments:
+            time_limit {[type]} -- [description] (default: {None})
+            gap {[type]} -- [description] (default: {None})
+            verbose {bool} -- [description] (default: {False})
+        
+        Returns:
+            [type] -- [description]
+        """
+        start = time.time()
+        # add vertex weights
+        for u in graph:
+            graph.nodes[u]['weight'] = profits[u]
+
+        #compute the adjacency matrix
+        adj_0 = nx.adj_matrix(graph).todense()
+
+        IS = -np.ones(adj_0.shape[0])
+
+        #pesi cambiati di segno
+        weights = np.array([graph.nodes[u]['weight'] for u in graph.nodes])
+        weighted_degrees = weights.dot(adj_0!=0)/weights
+        wgnodes = []
+        for i in range(len(graph.nodes)):
+            wgnodes.append(WGHeuNode(i, weighted_degrees[0,i]))
+        wgnodes.sort()
+
+        wgnode_index = 0
+
+        while np.any(IS==-1):
+            selected_node = wgnodes[wgnode_index]
+            selected_node_index = selected_node.node_index      
+            if IS[selected_node_index] != -1:
+                wgnode_index = wgnode_index + 1
+                continue                          
+            IS[selected_node_index] = 1 #choose the node
+            neighbors = np.argwhere(adj_0[selected_node_index,:]!=0) #find neighbors and excludes them from the solution
+            if neighbors.shape[0]:
+                IS[neighbors] = 0
+            wgnode_index = wgnode_index + 1
+        end = time.time()
+        of = np.array([graph.nodes[u]['weight'] for u in graph.nodes]).dot(IS)
+        return of, IS.tolist(), end - start,        
+
+
